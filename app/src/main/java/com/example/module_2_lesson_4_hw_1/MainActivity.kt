8faff8503f90e2190f13c9c6c2b14ae57c95517d
VD <vdfree448@protonmail.com>
2022-07-23 13:51:06 +0300

    added toast with button text

--- app/src/main/java/com/example/module_2_lesson_4_hw_1/MainActivity.kt
+++ app/src/main/java/com/example/module_2_lesson_4_hw_1/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.module_2_lesson_4_hw_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvNewGame.setOnClickListener {
+            val toast = Toast.makeText(applicationContext,R.string.new_game,Toast.LENGTH_SHORT)
+            toast.show()
+        }
+        tvContinue.setOnClickListener {
+            val toast = Toast.makeText(applicationContext,R.string.continue1,Toast.LENGTH_SHORT)
+            toast.show()
+        }
+        tvOptions.setOnClickListener {
+            val toast = Toast.makeText(applicationContext,R.string.options,Toast.LENGTH_SHORT)
+            toast.show()
+        }
+        tvAbout.setOnClickListener {
+            val toast = Toast.makeText(applicationContext,R.string.about,Toast.LENGTH_SHORT)
+            toast.show()
+        }
     }
 }
